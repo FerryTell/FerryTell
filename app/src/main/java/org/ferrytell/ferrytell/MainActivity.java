163c9db849979a35ecd06dcc5d533fe72f02d3be
Noel Lee <leekayau@gmail.com>
2018-09-01 13:22:19 +0800

    Initial commit

---
+++ app/src/main/java/org/ferrytell/ferrytell/MainActivity.java
@@ -0,0 +1,13 @@
+package org.ferrytell.ferrytell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
